8f8cf34bdea9bab2e84fea6ca747a5155feeeac7
jeremyloa <79776290+jeremyloa@users.noreply.github.com>
2023-04-23 18:03:07 +0700

    login and register

--- js/firebase/firebase.js
+++ js/firebase/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js'
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAEVUDjPK4hSwfij4BkJeydYyyAkfx61K4",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app)
