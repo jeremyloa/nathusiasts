fd2986f64a028d1fda35d9b523653b7fbab19e0a
jeremyloa <79776290+jeremyloa@users.noreply.github.com>
2023-04-23 20:59:42 +0700

    fix directory, add middleware, add logout temp

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { auth_middleware } from "./user.js"
+
+const acc_redir = document.getElementById('acc_redir')
+if (acc_redir) {
+    acc_redir.addEventListener("click", async (e)=>{
+        if (!(await auth_middleware())) window.location.assign('login-register.html')
+        else window.location.assign('account.html')
+    })
+}
+
+if (window.location.pathname === '/login-register.html') {
+    if (await auth_middleware()) window.location.assign('account.html')
+}
---
+++ js/user.js
@@ -0,0 +1,95 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, getAuth,onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js'
+import { getFirestore, doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js'
+
+const auth = getAuth()
+const db = getFirestore()
+
+export var user = null
+onAuthStateChanged(auth, (authUser) => {
+  if (authUser) {
+    getDoc(doc(db, "MasterUser", authUser.uid))
+    .then((doc) => user = doc.data())
+  } else {
+    user = null
+  }
+});
+
+export async function auth_middleware(){
+  let usr = await auth.currentUser
+  if (await usr && usr.uid !== "") return true
+  else return false
+}
+
+const reg_btn = document.getElementById('reg_btn')
+if (reg_btn) {
+  reg_btn.addEventListener("click", (e)=>{
+    e.preventDefault()
+    let name = document.getElementById('reg_name')
+    let mail = document.getElementById('reg_mail')
+    let pass = document.getElementById('reg_pass')
+    if (name.value  === "" || mail.value  === "" || pass.value  === "") alert('Fields should not be empty')
+    else if (name.value.length  < 8 || mail.value.length  < 8 || pass.value.length  < 8) alert('Length of each fields should be at least 8 characters')
+    else {
+      createUserWithEmailAndPassword(auth, mail.value , pass.value )
+        .then((creds) => {
+            updateProfile(creds.user, {displayName: name.value })
+            .then(() =>{ 
+              setDoc(doc(db, "MasterUser", creds.user.uid), {
+                name: name.value,
+              })
+              .then(() => {
+                alert('User succesfully registered. Please Sign In.')
+                name.value = ''
+                mail.value = ''
+                pass.value = ''
+              })
+            })
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+    }
+  })
+}
+
+const log_btn = document.getElementById('log_btn')
+if (log_btn) {
+  log_btn.addEventListener("click", async (e)=>{
+    e.preventDefault()
+    let mail = document.getElementById('log_mail')
+    let pass = document.getElementById('log_pass')
+    if (await auth_middleware()) alert('You are still logged in')
+    else if ( mail.value  === "" || pass.value  === "") alert('Fields should not be empty')
+    else if (mail.value.length  < 8 || pass.value.length  < 8) alert('Length of each fields should be at least 8 characters')
+    else {
+      signInWithEmailAndPassword(auth, mail.value, pass.value)
+      .then(() => {
+        window.location.assign('account.html')
+        mail.value = ''
+        pass.value = ''
+      })
+      .catch((e) => {
+        if (e.code === "auth/wrong-password" || e.code === "auth/invalid-email") {
+          alert("Invalid email or password.");
+        } else if (e.code === "auth/user-not-found") {
+          alert("User not found. Please check your email and try again.");
+        } else {
+          console.log(e.code, e.message);
+        }
+      })
+    }
+  })
+}
+
+const logout_btn = document.getElementById('logout_btn')
+if (logout_btn) {
+  logout_btn.addEventListener("click", (e)=>{
+    e.preventDefault()
+    signOut(auth)
+    .then(()=>window.location.assign('index.html'))
+    .catch((e)=>console.log(e))
+  })
+}
+
+
+
