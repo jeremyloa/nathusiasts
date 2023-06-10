7c51d3bbfa16ee90e7da1dc6cdb9df1c7ccae74e
jeremyloa <79776290+jeremyloa@users.noreply.github.com>
2023-06-10 13:28:51 +0700

    add: user transactions

--- js/cart.js
+++ js/cart.js
@@ -28,7 +28,32 @@ export const getMasterCartArray = () => {
         .catch(reject)
     })
 }
-  
+
+export const getTransactionCartArray = (transactionID) => {
+    return new Promise((resolve, reject) => {
+        getCurrUser()
+        .then((usr)=>{
+            const unsubscribe = onSnapshot(query(collection(db, "cart"), and(where("user", "==", usr.id), where("transaction", "==", transactionID))), (ss) => {
+                const data = []
+                ss.forEach((doc) => {
+                    data.push({
+                    id: doc.id,
+                    ...doc.data()
+                    })
+                })
+                if (data.length === 0) {
+                    unsubscribe(); 
+                    reject(new Error("No data found")); 
+                } else {
+                    unsubscribe(); 
+                    resolve(data); 
+                }
+            }, reject)
+        })
+        .catch(reject)
+    })
+}
+
 const item_qty = document.getElementById("item_qty")
 const item_id = document.getElementById("item_id")
 const item_stock = document.getElementById("item_stock")
